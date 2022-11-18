6f7eb7306878012a469f4c691db3df9e19b3b711
Quincy Miller <quibbles007@gmail.com>
2022-11-17 20:33:29 -0500

    finished initial frontent creation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="home">
-    HIIII MAP HERE
+    <v-container fluid>
+      <v-row class="d-block d-md-flex">
+        <v-col cols="12" md="3">
+          <ExpansionPanelVue />
+        </v-col>
+        <v-col cols="12" md="9">
+          <div id="mapView">
+            <!--This is where the mapbox map goes-->
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import ExpansionPanelVue from '@/components/ExpansionPanel.vue';
 
 export default {
   name: "HomeView",
   components: {
+    ExpansionPanelVue
   }
 };
 </script>
