18ec996c3c0e6ec75161db58ea0362cd97fdf4e1
Quincy Miller <quibbles007@gmail.com>
2022-12-29 11:17:30 -0500

    more changes to filters

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <v-select
-            :items="['All', 'Federal', 'State', 'Corporate', 'Other']"
+            :items="['All', 'Federal', 'State', 'Corporate', 'University', 'Non-profit', 'Other']"
             chips
             density="compact"
             variant="underlined"
@@ -17,6 +17,21 @@
             @change="filterResultsByCurrentState()"
             label="Fiscal Year" >
         </v-select>
+
+        <v-list id="result-list">
+            <v-item-group>
+                <v-list-item v-for="i in results" :key="i.fund_type ">
+                    <v-list-item-media>
+                        <v-list-item-title>Grant Name Here</v-list-item-title>
+                            <!-- <span v-if="current_year !== 'All'"><strong>{{current_year}}: </strong>{{ins['Award_Amount_' + current_year]}} </span>
+                            <span v-if="current_year == 'All'"><strong>Total Awards (2011-2022) </strong>{{ins['total_award']}} </span>
+
+                            <span v-if="current_year !== 'All'"><strong>{{current_year}} Jobs: </strong>{{ins['jobs_' + current_year]}} </span>
+                            <span v-if="current_year == 'All'"><strong> Total Jobs (2011-2022) </strong>{{ins['total_jobs']}} </span> -->
+                    </v-list-item-media>
+                </v-list-item>
+            </v-item-group>
+        </v-list>
     </div>
 </template>
 
