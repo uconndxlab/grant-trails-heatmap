6c13cc54f98c407e0c585f9f0418239689ce19d7
Quincy Miller <quibbles007@gmail.com>
2022-12-29 10:56:08 -0500

    filter UI works

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,25 +1,21 @@
 <template>
     <div>
         <v-select
-            v-model="current_type"
             :items="['All', 'Federal', 'State', 'Corporate', 'Other']"
-            @change="filterResultsByCurrentState()"
-            height="50"
             chips
-            dense
+            density="compact"
             variant="underlined"
+            @change="filterResultsByCurrentState()"
             label="Grant Type">
         </v-select>
 
         <v-select
             :items="['All', '2014', '2015', '2016', '2017', '2018', '2019', '2020']" 
-            chips 
-            label="Fiscal Year" 
-            height="50" 
-            dense
-            v-model="current_year"
+            chips
+            density="compact"
             variant="underlined"
-            @change="filterResultsByCurrentState()">
+            @change="filterResultsByCurrentState()"
+            label="Fiscal Year" >
         </v-select>
     </div>
 </template>
