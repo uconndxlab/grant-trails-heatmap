1ceda5b22ea1eb52bea6ec83c0417d6571e00bfd
Jack Medrek <jackmedrek@Jacks-MacBook-Pro.local>
2023-01-17 19:46:49 -0500

    added clearMarkers()

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -7,6 +7,8 @@
 import mapboxgl from "mapbox-gl";
 import { mapActions, mapGetters } from "vuex";
 
+var currentMarkers = [];
+
 export default {
     name: "MapVue",
     data() {
@@ -22,6 +24,7 @@ export default {
     },
     watch:{
         grants(){
+            this.clearMarkers(this.grants)
             this.addMarkers(this.grants)
         }
     },
@@ -52,11 +55,10 @@ export default {
         async addMarkers(grantObj) {
             console.log('Adding markers!')
             for (const grant of grantObj) {
-                //grant = JSON.parse(JSON.stringify(grant)); //convert proxy objects to arrays
                 console.log(grant.grants_location);
 
                 const el = document.createElement("div");
-                const diam = 20 + 0.01 * Math.sqrt(grant.total_amount);
+                const diam = 20 + 0.8 * Math.pow(grant.total_amount, 1/4);
                 el.className = "marker";
                 el.style.width = `${diam}px`;
                 el.style.height = `${diam}px`;
@@ -64,6 +66,14 @@ export default {
                 this.marker = new mapboxgl.Marker(el)
                     .setLngLat(JSON.parse(grant.grants_location).reverse())
                     .addTo(this.map);
+                currentMarkers.push(this.marker);
+            }
+        },
+        async clearMarkers() {
+            if (currentMarkers !== null) {
+                for (var i = currentMarkers.length - 1; i >= 0; i--) {
+                    currentMarkers[i].remove();
+                }
             }
         }
     }
