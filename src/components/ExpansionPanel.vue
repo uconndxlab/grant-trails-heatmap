0c740d9b551133bc33d4bae009b1c7e1d2f61edc
Quincy Miller <quibbles007@gmail.com>
2023-02-01 15:35:19 -0500

    small commit

--- src/components/ExpansionPanel.vue
+++ src/components/ExpansionPanel.vue
@@ -23,12 +23,12 @@
                     It's not 100% of the data because there were logistical issues that made getting some of the data challenging. That said, we have no reason to believe that the missing data would not be proportionally distributed, so the relative circle sizes shouldn't really change (though the exact dollar amounts might somewhat)-->
                     <p>What you're looking at here is a visualization of where approximately 85% of direct grant dollars received by UConn faculty were spent within Connecticut between fiscal years 2020 to 2021.</p>
                     <p>
-                        Feel free to explore the visualization by typing in a location (city or zip), adjusting filters, or just panning and zooming on the map. Most importantly <a href="https://squaredlabs.uconn.edu/">let us know</a> what you think!
+                        Feel free to explore the visualization by typing in a location (city or zip), adjusting filters, or just panning and zooming on the map. Most importantly <a href="https://dxgroup.core.uconn.edu/">let us know</a> what you think!
                     </p>
 
                     <!--TODO: Add <a> tags to necessary spots below (github, license)-->
                     <h4> Credit</h4>
-                    <p>Grant Trails is an open source project available for universities to showcase their geographic grant distribution. Source code can be found on GitHub.</p>
+                    <p>Grant Trails is an open source project available for universities to showcase their geographic grant distribution. Source code can be found on <a href="https://github.com/uconndxlab/grant-trails-heatmap">GitHub</a>.</p>
                     <p>The project is built using an extensive library of open data and software. A brief list can be found in the copyright and license file.</p>
                 </v-expansion-panel-text>
             </v-expansion-panel>
